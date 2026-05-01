d81033ed23a443867f91529fe625ad336f377bbd
Alexa-star1 <zone902767@gmail.com>
2026-05-01 15:41:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const products = [
     image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf"
   }
 ];
-
+const products = [
+  {name:"LUXYRA Street Tee",price:599,image:"..."},
+  {name:"LUXYRA Hoodie",price:999,image:"..."},
+  {name:"Oversized Tee",price:699,image:"https://images.unsplash.com/photo-1503342217505-b0a15ec3261c"},
+  {name:"Street Pants",price:1199,image:"https://images.unsplash.com/photo-1523381210434-271e8be1f52b"}
+];
 const div = document.getElementById("products");
 
 if (!div) {
